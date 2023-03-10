73dc0d5e40a47f0245ec55287f7c59bc2d75dbc7
Daniel Gonzalez <dani.gg83@gmail.com>
2023-03-10 17:38:54 +0100

    gate admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Access\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function(User $user) {
+            //dd(auth()->user());
+            return  auth()->user()->admin
+            ? Response::allow()
+                : Response::deny('You must be an administrator.');;
+        });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" type="image/x-icon" href="https://www.amorfa.cat/wp-content/uploads/favicon.ico">
 
-        <title>Laravel</title>
+        <title>AMPA-Albaicín</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
