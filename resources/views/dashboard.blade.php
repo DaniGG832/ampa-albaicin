f62287575a5f40b19583162961b13e21c05ce1ce
Daniel Gonzalez <dani.gg83@gmail.com>
2023-04-12 11:36:19 +0200

    botones vista inicio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,9 +20,41 @@
                         </div>
                     @endif
 
-                   
-                {{auth()->user()->is_admin()}}
+
+                    {{-- {{ auth()->user()->is_admin() }} --}}
                     {{-- {{auth()->user()->activado}} --}}
+
+                    <div class="flex flex-col">
+
+                        <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+
+                                {{-- {{request()->get('rq')}} --}}
+
+                                <div class=" flex justify-center ">
+
+                                    <div class=" flex flex-col items-center">
+
+
+                                        <div class="m-5"><a href="{{ route('movimientos.index') }}"
+                                                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
+                                                href="">Movimientos</a></div>
+
+                                        <div class="m-5"><a href="{{ route('admin.index') }}"
+                                                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
+                                                href=""> Menu Admin</a></div>
+
+                                        {{-- <div class="m-5"><a href="{{ route('admin.admin') }}"
+                                                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
+                                                href="">Activar/Desactivar Usuarios Admin</a></div> --}}
+
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
