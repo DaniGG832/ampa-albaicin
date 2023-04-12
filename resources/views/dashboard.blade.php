48b773d142cd1b785fa53a4c728481eccd88d2c4
Daniel Gonzalez <dani.gg83@gmail.com>
2023-04-12 11:49:21 +0200

    if para el boton menu admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,9 +40,12 @@
                                                 class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
                                                 href="">Movimientos</a></div>
 
-                                        <div class="m-5"><a href="{{ route('admin.index') }}"
-                                                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
-                                                href=""> Menu Admin</a></div>
+                                                @if (auth()->user()->admin)
+                                                    
+                                                <div class="m-5"><a href="{{ route('admin.index') }}"
+                                                        class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
+                                                        href=""> Menu Admin</a></div>
+                                                @endif
 
                                         {{-- <div class="m-5"><a href="{{ route('admin.admin') }}"
                                                 class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500"
